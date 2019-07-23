85ac024510acd50d0c9de90db753c97a61596c01
zeinab taghavi <zeinabtaghavi1377@gmail.com>
2019-07-23 16:02:21 +0430

    source image and .hOCR is needed

---
+++ rect_lines_with_hOCR.py
@@ -0,0 +1,24 @@
+from lxml import etree
+import cv2
+
+dpi = (300,300) # default is (300,300) but you can check with PIL, Image.info
+try:
+    f = open('z1.hocr', 'r', encoding='iso-8859-1').read().encode('utf-8')  # 'z1.hocr'
+except:
+    print('hOCR file was not found')
+
+
+imgTemp = cv2.imread('image1_Modified.jpg') # image file name
+
+tree = etree.fromstring(f)
+words = tree.xpath("//*[@class='ocr_line']")
+
+lines_Direction = []
+
+for w in words:
+    titles = w.attrib['title'].split()
+    x1, y1, x2, y2 = int(titles[1]), int(titles[2]), int(titles[3]), int(titles[4].split(';')[0])
+    imgTemp = cv2.rectangle(imgTemp , (x1 , y1) , (x2,y2) , (255,0,0) , 3)
+
+
+cv2.imwrite('rect_lines_with_hOCR.jpg',imgTemp)

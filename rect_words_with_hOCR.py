8aed254a286293e27a6d9079a6268478697248f7
zeinab <zeinabtaghavi1377@gmail.com>
2019-09-01 14:53:43 +0430

    no need to give it .hocr, it will find it itself:)

--- rect_words_with_hOCR.py
+++ rect_words_with_hOCR.py
@@ -1,9 +1,9 @@
 from lxml import etree
 import cv2
+import pytesseract
 
-img = cv2.imread('image1.bmp')
-f = open('z1.hocr' , 'r', encoding='iso-8859-1').read().encode('utf-8')
-
+img = cv2.imread('EAST_on_denoised_img.png')
+f = pytesseract.pytesseract.image_to_pdf_or_hocr(img , lang='fas+ara',extension='hocr')
 tree = etree.fromstring(f)
 words = tree.xpath("//*[@class='ocrx_word']")
 for w in words:
@@ -14,4 +14,4 @@ for w in words:
 
 # gray2 = cv2.cvtColor(img_hocr , cv2.COLOR_BGR2GRAY)
 # final = cv2.bitwise_and(new_image , final_img)
-cv2.imwrite('rect_words_with_hOCR.jpg',img_hocr)
+cv2.imwrite('rect_words_with_hOCR_EAST_on_denoised_img.jpg',img_hocr)
